f5328153eb500c671e63060663e53cfc049e52f8
Trung C. Nguyen <nguyencanhtrung@me.com>
2016-07-28 16:51:49 +0200

    Update new

--- samples/[4]mmult_optimized_apf/mmult_accel.h
+++ samples/[4]mmult_optimized_apf/mmult_accel.h
@@ -6,6 +6,8 @@
 #define B_NCOLS 32
 #define B_NROWS A_NCOLS
 
+
+/*< Making change here */
 #pragma SDS data access_pattern(in_A:SEQUENTIAL, in_B:SEQUENTIAL, out_C:SEQUENTIAL)
 void mmult_accel (float in_A[A_NROWS*A_NCOLS],
                   float in_B[A_NCOLS*B_NCOLS],
